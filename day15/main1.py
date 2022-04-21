57258ea908be273880d1646c5f807b68ef1a3dd0
luciano <amunnezza@gmail.com>
2022-04-21 20:02:20 +0200

    fatti alcuni cambiamenti a day15

---
+++ day15/main1.py
@@ -0,0 +1,49 @@
+#MENU MODELLA I TRE TIPI DI BEVANDA CON COSTO, INGREDIENTI
+MENU = {
+
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+#QUI MODELLA LE RISORSE CHE LA MACCHINA PUO CONTENERE
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+
+
+
+# TODO: 1. Print a Report of caffe machine reource
+
+# TODO : 2 Check if resource are sufficient
+
+# TODO PROCESS COIN 
+
+# TODO CHECK TRANSACTION SUCCESSFULL 
+
+# TODO MAKE COFFEE (UPDATING RESOURCES)
+
+# FIXME 
