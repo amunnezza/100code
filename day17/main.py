939690c3d22867b4295e38501ed077527c18df90
Luciano <amunnezza@gmail.com>
2022-04-23 13:03:24 +0200

    quasi finito day17

--- day17/main.py
+++ day17/main.py
@@ -1,76 +1,26 @@
-#supponi di avere sito web e ti serve un oggetto user
+#from quiz_brain import QuizBraint
 
-# class User:
-#     pass
-#     #questo non fa niente
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+for dato in question_data :
+    new_question = Question (dato["text"], dato["answer"])
+    question_bank.append(new_question)
+# per testprint (question_bank)
+#fine 157 torna a cherrytree
 
-# utente_1 = User()
-# #pero funziona
-# print (utente_1)
-#2 osserva che pratica comune scrivere nome delle classi con PascalCase
-# per es class NomeDellaClasse:
-         #   eccetera
-# 3 diverso da camelCase perchè in camel il primo minuscole e le altre maiuscole poco usato in pyhton
-# esitste anche snake_case usato per tutto tranne nome classi     
-# 4 nota che potrei ora aggiungere attributi all'oggetto creato 
-# per esempio leggittimo fare
-# utente_1.id = "001"
-# utente_1.username = "luciano"
-# print (utente_1.id)     
-#ma se per ogni object devi creare sempre le stesse attribute?
-#5 entra in gioco constructo per starting point 
-#initialize 
-#constructor is a special function and python know this because this method have double underscore
-#to beginning and end. Is something like this
-class Car:
-    def __init__(self, seats):
-        #here you can initialize attribute
-        # pass
-        self.seats = seats
+#PUNTO 1 venendo da quiz_brain.py
 
-my_car = Car(5) # 5 is the variable seats of the object 
-#6 **importante** init called every time i create an object
-#7 applichiamo a user
-# class User :
-#     def __init__(self, user_id, username) :
-#         self.id = user_id
-#         self.username = username
-#         self.follower = 0
-#     pass
-
-
-#print (utente_1.username + " " + utente_1.id )
-#osserva con constructor devi poi fornire i parametri richiesti quando crei oggetto
-# posso anche fissare dei valori di default per gli attributi come ad esempio follower sopra 
-#e funziona come vedi 
-#print (f"attualmente il numero di follower è {utente_1.follower}")
-
-#10 vai a cherrytree 
-
-#torna da cherryTree
-
-class User :
-    def __init__(self, user_id, username) :
-        self.id = user_id
-        self.username = username
-        self.follower = 0
-        self.following = 0
-    def follow (self, user):   #nota che self ci va sempre cosi il metodo sa che deve laroare sulloggetto
-        """vuole come parametro un utente della Classe User da seguire""" 
-        user.follower +=1
-        self.following += 1 #incrementa di uno i follower di quello che segui e aumenti il tuo numero
-                            #di quelli che stai seguendo
-utente_1 = User("001", "luciano")
-utente_2 = User ("002", "francesca")
-utente_1.follow(utente_2)    
-print (utente_1.follower)
-print ("dovrebbe essere 0")
-print (utente_1.following)
-print ("dovrebbe essere 1")
-print (utente_2.follower)
-print ("dovrebbe essere 1")
-print (utente_2.following)
-print ("dovrebbe essere 0")
-#fine per questo file inizia progetto di prova
-#vai a chettyTree
+quiz = QuizBrain(question_bank)
+#TORNA A QUIZBRAIN.PY
+#PUNTO 2 
+quiz.next_question()      
+# e fino a qui interfaccia funziona come volevo
+# fine video 158
+#mettiamo ora una cosa del TIPo 
+# while (if quiz has still question da fare in quizbrain)
+# quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
 
--- day17/question_model.py
+++ day17/question_model.py
@@ -1,5 +1,7 @@
+#create a Question class with an init method with two attributes TEXT and ANSWER
+
 class Question :
-    def __init__(self, q_text, q_answer) :
-        self.text = q_text
-        self.answer = q_answer
-       
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+#termina il primo task e continua su cherryTree
