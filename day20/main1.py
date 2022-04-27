a216c969e22c85e6148d1dd64ec3cf41e11dae60
luciano <amunnezza@gmail.com>
2022-04-27 20:53:18 +0200

    fine day 20

--- day20/main1.py
+++ day20/main1.py
@@ -1,25 +1,21 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
+from snake import Snake
 screen = Screen()
 screen.setup( width= 600, height = 600)
 screen.bgcolor("black") 
 screen.title("My snake game")
 screen.tracer(0) # annulla aggiornamento continuo
 
-#1 create a snake body
-#every turtle is 20 x 20 and get 3 squre adjacent
-starting_position =[(0,0), (-20,0), (-40,0)]
-segments = []
-for position in starting_position :
-    new_segment = Turtle("square")
-    new_segment.color ("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-
+snake = Snake()
 game_is_on = True
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right, "Right")
 while game_is_on:
     screen.update() #per dare senso di movimento
     # for seq in segments:
@@ -28,12 +24,7 @@ while game_is_on:
     #il codice che segue basa tutto sul fatto che parti dall'ultimo e gli fai 
     #assumere la posizione del precedente e cosi via su tutta la catena con il
     #primo che è l'ultimo a muoversi e detta lui la direzione del movimento!!!
-    for seg_num in range (len(segments)-1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)  
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
 
 
 #per girare fai assumere all'ultimo la posizione del precedente e cosi via 
