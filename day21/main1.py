ee7e8b4d193efc69979853246f24f551971abc73
luciano <amunnezza@gmail.com>
2022-04-28 22:57:58 +0200

    end day 21

--- day21/main1.py
+++ day21/main1.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup( width= 600, height = 600)
@@ -11,6 +12,7 @@ screen.tracer(0) # annulla aggiornamento continuo
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 game_is_on = True
 
 screen.listen()
@@ -28,10 +30,25 @@ while game_is_on:
     #primo che è l'ultimo a muoversi e detta lui la direzione del movimento!!!
     snake.move()
     # how to detect c ollision? usa metodo distance
-    if snake.head.distance (food) < 15:
+    if snake.head.distance (food) < 20:  #method distance work if argument is a turtle objiece
         #print ("they collide") # test funzionamento
         #in realta dovrebbe creare un nuovo pallino
+        #per farlo crea un nuovo metodo in food che si chiama refresh()
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    #DETECT COLLIZION WITH WALL
+
+    if snake.head.xcor()  > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        scoreboard.game_over()
+        game_is_on = False 
         
+    #detect collision
+    # if head hit any other part of snake game over
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()     
 
 #per girare fai assumere all'ultimo la posizione del precedente e cosi via 
 #cambi il modo di muoversi e se gira gli altri losegueno
