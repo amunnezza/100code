81493e0d039b6cff2ef3ab51930c570566199a68
luciano <amunnezza@gmail.com>
2022-05-01 00:26:07 +0200

    finito day 22

--- day22/day22.py
+++ day22/day22.py
@@ -1,14 +1,19 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PONG")
 screen.tracer(0)
+from score import Scoreboard
 
 
 r_paddle = Paddle ((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Scoreboard()
 # paddle = Turtle()
 # paddle.shape("square")
 # paddle.color ("white") 
@@ -35,7 +40,25 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move() 
+    # detect the collision
+    if ball.ycor() > 280 or ball.ycor() < -280 :
+        #ball should bounce() ball.bounce()
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 335 or (ball.distance(l_paddle) < 50 and ball.xcor() < -335) :
+        ball.bounce_x()
+    
+    if ball.xcor() > 380: 
+        ball.reset_position()
+        score.l_point()
+
+    if ball.xcor() < -380 :
+        ball.reset_position()
+        score.r_point()
+
+
 
 
 
