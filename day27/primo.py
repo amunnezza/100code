9754b7151ad35ab83062cf0a6afc73b1aaea1cdb
luciano <amunnezza@gmail.com>
2022-05-08 22:59:37 +0200

    inizio day 27

---
+++ day27/primo.py
@@ -0,0 +1,16 @@
+import tkinter
+
+from numpy import MAY_SHARE_BOUNDS
+
+window = tkinter.Tk()
+window.title("My first gui program")
+window.minsize( width= 500, height= 300)
+#LABER Nota per qualuncqu componente devi
+#crearlo e poi vedere dove posizionarlo
+my_label = tkinter.Label(text = "I m a label", font=("Arial", 24,"bold"))
+#creato la label
+my_label.pack() # lo posizioni al centro
+
+
+
+window.mainloop() #loop in attesa di eventi
