eab319bb1dfe074ab65f8a623666d9d84e10ece6
Luciano <amunnezza@gmail.com>
2022-05-20 18:46:53 +0200

    inizio day 30

---
+++ day30/main.py
@@ -0,0 +1,15 @@
+with open("./day30/prova.txt") as file:
+    result = file.read()
+    print (result)
+#FileNotFoundError
+# with open ("./day30/nonEsiste.txt") as file2:
+#     file2.read()
+
+
+# KeyError
+# a_dict = {"key":"value"}
+# value = a_dict["nonExistentKey"]
+
+# IndexError
+# fruit_list = ["banana", "mela", "arancia"]
+# print (fruit_list[3])
