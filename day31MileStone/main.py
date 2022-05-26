7ae81a414294c63ea6f8afb7d101c84cc834f41f
luciano <amunnezza@gmail.com>
2022-05-26 20:52:23 +0200

    inizio

--- day31MileStone/main.py
+++ day31MileStone/main.py
@@ -1,2 +1,12 @@
+from tkinter import * #1
 BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk() #2
+window.title("Flashy")  #3
+window.config (padx=50, pady=50, bg=BACKGROUND_COLOR) #4
+
+
+
+window.mainloop()
+
 
