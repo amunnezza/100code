72202942efe08bf5d929b78cec9646227974dda8
luciano <amunnezza@gmail.com>
2022-06-05 21:12:50 +0200

    end day 34

--- day34/Solution/main.py
+++ day34/Solution/main.py
@@ -1,5 +1,3 @@
-PATH = 'C:\\WPy64-3940\\codice'
-
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -15,10 +13,3 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 quiz_ui = QuizInterface(quiz)
-
-
-# while quiz.still_has_questions():
-#     quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
