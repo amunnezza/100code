23f881559f3d1e5d4cfc2243c8bf6de5a9715741
Luciano Ciotola <amunnezza@gmail.com>
2022-06-08 15:36:32 +0200

    fine giorno 35

--- day35/secondo.py
+++ day35/secondo.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "f33927f8fb25fd8a542159d17e2429b4"
 weather_params = {
@@ -7,6 +8,11 @@ weather_params = {
     "appid":api_key,
     "exclude":"current,minutely,daily"
 }
+TWILIO_ACCOUNT_SID = "AC90490eb4bde8f275476f7088706b0e2f"
+TWILIO_AUTH_TOKEN = "43baaec2d8c5743ee30dcabad37470aa" 
+
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_AUTH_TOKEN
 response = requests.get(OWM_endpoint, params=weather_params)
 #print(response.status_code) di test per verificare 200
 response.raise_for_status() #check se ha problemi
@@ -14,7 +20,7 @@ weather_data = response.json()
 # with open( "prova.json", "w") as file:
 #     file.write(str(weather_data))
 weather_slice = weather_data["hourly"][:12]
-print (weather_slice)
+#print (weather_slice)
 # print (weather_data["hourly"][0]["weather"][0]["id"])
 will_rain = False
 for hour_data in weather_slice:
@@ -23,7 +29,14 @@ for hour_data in weather_slice:
         #print ("bring an umbrella") stampa troppe volte
         will_rain = True
 if will_rain:
-    print ("bring an umbrella")
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages \
+        .create(
+            body="It's going to rain today Remember the umbrella.", 
+            from_= '+15013226175',
+            to='+39 331 360 8213'
+        )
+    print (message.status)
 
 
 
