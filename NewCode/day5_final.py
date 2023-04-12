d4c200033813a19bacc3ad9be4b518cbcab27a1f
amunnezza <amunnezza@gmail.com>
2023-04-12 08:08:39 +0200

    finito day 5

--- NewCode/day5_final.py
+++ NewCode/day5_final.py
@@ -28,4 +28,27 @@ for indice in range(nr_numbers):
 print (password)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password1 = ""
+rand_choice = random.randint(0,2)
 
+list_rimanenze = [nr_letters, nr_symbols, nr_numbers]
+len_password = nr_letters + nr_symbols + nr_numbers
+while len_password != 0:
+    rand_choice = random.randint(0,2)
+    if rand_choice == 0 and list_rimanenze [0] != 0:
+        password1 += letters[random.randint(0, letters_len -1)]
+        len_password += -1
+        list_rimanenze[0] = list_rimanenze[0] -1 
+    elif rand_choice == 1 and list_rimanenze [1] != 0:
+        password1 += symbols[random.randint(0, symbols_len -1)]
+        len_password += -1
+        list_rimanenze[1] = list_rimanenze[1] -1  
+    elif rand_choice == 2 and list_rimanenze [2] != 0:
+        password1 += numbers[random.randint(0, numbers_len -1)]
+        len_password += -1
+        list_rimanenze[2] = list_rimanenze[2] -1  
+    else:
+        continue
+
+print (password1)
+        
