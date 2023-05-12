98ef11787d2b591dca4ec953fcbaf3618820fa01
amunnezza <amunnezza@gmail.com>
2023-05-12 18:36:06 +0200

    fine gg 20

--- NewCode/day20/snake_mio.py
+++ NewCode/day20/snake_mio.py
@@ -1,9 +1,15 @@
 STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0) ]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 from turtle import Turtle
 class Snake:  #PUNTO 1 per tradizione la classe maiuscola
     def __init__(self) -> None:
         self.segments = []
         self.create_snake() #PUNTO 2 il primo metodo della classe sara quindi create_snake
+        self.head = self.segments[0]
     
     def create_snake(self):    #simile a quanto  gia visto
         for position in STARTING_POSITIONS:
@@ -17,7 +23,38 @@ class Snake:  #PUNTO 1 per tradizione la classe maiuscola
     #PUNT 3 adesso devi vedere il metodo move per seguire la testa di snake ed ho
 
     def move (self):
-        
+        for seg_number in range (len(self.segments)-1, 0, -1):
+            new_x = self.segments[seg_number-1].xcor()
+            new_y = self.segments[seg_number-1].ycor()
+            self.segments[seg_number].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+    
+    
+    def up(self):
+        if self.segments[0].heading() == DOWN:  #QU3ESTI PER NON TORNARE SUI PROPRI PASSI
+            pass
+        else:
+            self.segments[0].setheading(UP)  
+    
+    def down(self):
+        if self.segments[0].heading() == UP:
+            pass
+        else:
+            self.segments[0].setheading(DOWN)  
+
+    def left(self):
+        if self.segments[0].heading() == RIGHT:
+            pass
+        else:
+            self.segments[0].setheading(LEFT)  
+    
+    def right (self):
+        if self.segments[0].heading() == LEFT:
+            pass
+        else:
+            self.segments[0].setheading(RIGHT)  
+
+
 
 
         
