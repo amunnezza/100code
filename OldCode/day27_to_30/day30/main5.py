b2bba33055b7941cd5ed273a028a6ec0cf3b9a28
luciano <amunnezza@gmail.com>
2023-06-09 23:02:24 +0200

    eliminati alcuni file da old code

--- OldCode/day27_to_30/day30/main5.py
+++
@@ -1,8 +0,0 @@
-height = float (input("insert the height\n"))
-weight = int (input ("insert your weight\n"))
-if height > 2.5:
-    raise ValueError ("l'altezza umana non dovrebbe superare i 2.5 metri")
-#quanto sopra mi dice quando potrebbe essere utile un raise mio, in fase di check 
-
-bmi = weight /(height**2)
-print (bmi)
