57e6b66f32086af1e3405fcb001409b9b2155fbc
amunnezza <amunnezza@gmail.com>
2024-05-10 12:36:41 +0200

    piccola prova

---
+++ NewCode/quadrato.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.bgcolor ("blue")
+
+squary = turtle.Turtle()
+squary.speed = 20 
+squary.pencolor("yellow")
+
+for i in range (400):
+    squary.forward(i)
+    squary.left(91)
+
+    
